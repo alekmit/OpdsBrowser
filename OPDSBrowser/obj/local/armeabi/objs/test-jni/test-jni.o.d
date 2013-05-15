e28ef6cb75a390b752e7400e4b18bd28726e384a
alekmit <alekmit@gmail.com>
2013-05-15 19:11:45 +0300

    jni test

---
+++ OPDSBrowser/obj/local/armeabi/objs/test-jni/test-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/test-jni/test-jni.o: jni/test-jni.c \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/string.h \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/malloc.h \
+ C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/jni.h
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk-r8e/platforms/android-4/arch-arm/usr/include/jni.h:
